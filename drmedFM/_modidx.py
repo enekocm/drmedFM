de9ebec30d97305be0de7391571a1acfe8ca8354
enekocm <enekocm21@gmail.com>
2024-08-01 16:18:28 +0200

    third  version

--- drmedFM/_modidx.py
+++ drmedFM/_modidx.py
@@ -7,5 +7,4 @@ d = { 'settings': { 'branch': 'master',
                 'lib_path': 'drmedFM'},
   'syms': { 'drmedFM.Datamodule': { 'drmedFM.Datamodule.hellosum': ('datamodule.html#hellosum', 'drmedFM/Datamodule.py'),
                                     'drmedFM.Datamodule.hellosum2': ('datamodule.html#hellosum2', 'drmedFM/Datamodule.py')},
-            'drmedFM.Dataset': {'drmedFM.Dataset.foo': ('dataset.html#foo', 'drmedFM/Dataset.py')},
-            'drmedFM.core': {'drmedFM.core.foo': ('dataset.html#foo', 'drmedFM/core.py')}}}
+            'drmedFM.Dataset': {'drmedFM.Dataset.foo': ('dataset.html#foo', 'drmedFM/Dataset.py')}}}
